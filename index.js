b71fedee00b4f2c94937ad3e79e58b3612ba7609
Vikalp Malhotra <kimboho@gmail.com>
2021-12-13 11:38:03 -0500

    commit tree 927cc03cffbbd01e7eca8d1779100ef1ce00121c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+
+let currentUser = 'Biggie Smalls'
+
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+
+let excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase() + `${currentUser} + !`;
+
+let shortGreeting = 'Welcome, ' + currentUser.slice(0, 1) + '!';
+
